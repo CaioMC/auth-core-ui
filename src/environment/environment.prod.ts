98fefb6ffd5d30bde672b22fa678f122368f130f
caio.coelho <caio.coelho@totvs.com.br>
2023-11-27 16:45:22 -0300

    merge

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    url: 'http://localhost:8080',
+    url: 'http://ec2-18-191-212-243.us-east-2.compute.amazonaws.com:8080',
     urlAuth: ' http://ec2-3-137-212-233.us-east-2.compute.amazonaws.com:8080'
 };
