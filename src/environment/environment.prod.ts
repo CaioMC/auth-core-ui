f4121bf82314ad27457e8af952daf725a0cfa324
Caio Miranda Coelho <65717443+CaioMC@users.noreply.github.com>
2023-12-06 23:10:14 -0300

    Update environment.prod.ts

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    url: 'http://ec2-15-228-149-12.sa-east-1.compute.amazonaws.com/:8080',
+    url: 'http://ec2-15-228-149-12.sa-east-1.compute.amazonaws.com:8080',
     urlAuth: ' http://ec2-18-231-170-132.sa-east-1.compute.amazonaws.com:8080'
 };
