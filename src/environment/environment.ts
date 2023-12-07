540c53031a41620d77f2c9e1a2d403396043b9b0
caio.coelho <caio.coelho@totvs.com.br>
2023-12-06 22:59:44 -0300

    ajuste url

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    url: 'http://ec2-18-191-212-243.us-east-2.compute.amazonaws.com:8080',
-    urlAuth: ' http://ec2-3-137-212-233.us-east-2.compute.amazonaws.com:8080'
+    url: 'http://ec2-15-228-149-12.sa-east-1.compute.amazonaws.com/:8080',
+    urlAuth: ' http://ec2-18-231-170-132.sa-east-1.compute.amazonaws.com/:8080'
 };
